4015a05c4a4bf9f131b026bee61d91a6fec17364
nivav1233 <nivav1233@gmail.com>
2024-10-29 17:31:39 +0200

    bugfix

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css'
+
+
+export default function Home() {
+    return (
+    <div>
+    <h1 id='homemsg' >WELCOME! (user-pull from db)</h1>
+    </div>
+    )
+}
