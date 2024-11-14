87e0dbfef570216a41e896d1afb3dd9d7d078255
nivav1233 <nivav1233@gmail.com>
2024-11-14 12:39:25 +0200

    test-without-terraform

--- machine_ip.sh
+++ machine_ip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-machine_ip=$(terraform output -raw web_public_ip)
+# machine_ip=$(terraform output -raw web_public_ip)
+
+machine_ip=51.17.155.68
 
 echo "$machine_ip ansible_user=admin"  > ./host.ini
 
