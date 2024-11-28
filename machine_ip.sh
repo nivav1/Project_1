e26396a1cfdd7013cbb55185145406591ed627f6
nivav1233 <nivav1233@gmail.com>
2024-11-28 17:43:19 +0200

    correct user

--- machine_ip.sh
+++ machine_ip.sh
@@ -2,6 +2,6 @@
 
 machine_ip=$(terraform output -raw web_public_ip)
 
-echo "$machine_ip ansible_user=admin"  > ./ansible/host.ini
+echo "$machine_ip ansible_user=root"  > ./ansible/host.ini
 
 echo $machine_ip
